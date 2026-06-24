fa44e8c3f8afe3c1284f6a3e9eb89d31423da7e8
sebastienlehucher1 <seb.lehucher@gmail.com>
2026-06-24 21:43:40 +0200

    modifs docker compose

--- namenode-init.sh
+++ namenode-init.sh
@@ -7,10 +7,24 @@ until hdfs dfs -ls / >/dev/null 2>&1; do
 done
 
 # Attendre la fin du Safe Mode
-until hdfs dfsadmin -safemode get 2>/dev/null | grep -q "OFF"; do
-  echo "Waiting for NameNode to leave Safe Mode..."
-  sleep 3
+echo "Waiting for Safe Mode to exit..."
+
+for i in $(seq 1 60); do
+  MODE=$(hdfs dfsadmin -safemode get 2>/dev/null || echo "UNKNOWN")
+
+  if echo "$MODE" | grep -q "OFF"; then
+    echo "Safe Mode OFF"
+    break
+  fi
+
+  echo "Still in Safe Mode..."
+  sleep 5
 done
+
+# Si safemode toujours ON après timeout → on continue quand même
+MODE=$(hdfs dfsadmin -safemode get 2>/dev/null || echo "UNKNOWN")
+echo "Final SafeMode state: $MODE"
+
 
 # Créer /apps/tez avec les bonnes permissions
 hdfs dfs -mkdir -p /apps/tez
